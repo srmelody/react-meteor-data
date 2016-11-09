2bcc1cd0266aff3564d495578ab0f6c531cac13a
Johan Olsson <johan.olsson@attentec.se>
2016-11-09 12:55:59 +0100

    add more detailed types to createContainer

--- main.d.ts
+++ main.d.ts
@@ -1,8 +1,17 @@
 // Type definitions for meteor package react-meteor-data.
 // Project: https://atmospherejs.com/meteor/react-meteor-data
 // Definitions by:
-// <https://github.com/fullflavedave>
+// <https://github.com/fullflavedave> and <https://github.com/kurnos>
+
+///<reference path="typings/globals/react/index.d.ts"/>
 
 declare module 'meteor/react-meteor-data' {
-  export function createContainer(func: () => { [name: string]: any }, reactComponent: any): any;
+  import * as React from 'react';
+
+  type ComponentConstructor<P> = React.ComponentClass<P> | React.StatelessComponent<P>
+
+  export function createContainer<InP, D, OutP extends (InP & D)>(
+    options: (props: InP) => D | {getMeteorData: (props: InP) => D, pure?: boolean},
+    reactComponent: ComponentConstructor<OutP>)
+    : ComponentConstructor<InP>;
 }
